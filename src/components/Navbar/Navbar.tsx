9da660c83a32998bb9a0a0cb122dd0736a2be337
ganesh bhatt <@ganesh.bhatta>
2024-03-30 16:47:39 +0545

    feat: Nav bar added..

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import cart from "@/assets/cart.svg";
+import { useShoppingCart } from "@/context/ShoppingCartContxt";
+import Image from "next/image";
+
+const Navbar: React.FC = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
+  
+  return (
+    <div className="flex justify-between p-5 bg-lightGreen sticky">
+      <h3 className='text-white'>E-store</h3>
+      <div className="flex relative border-2 p-2 rounded-full" onClick={openCart}>
+        <Image src={cart} alt="cart-image" width={22} height={22} />
+        {cartQuantity > 0 && (
+          <button>
+            <h3 className="absolute bottom-[-2] right-0 text-white">
+              {cartQuantity}
+            </h3>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
