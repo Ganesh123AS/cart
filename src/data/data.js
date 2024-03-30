d360607ceeca71132e359f30bf9741c1d08c8dc7
ganesh bhatt <@ganesh.bhatta>
2024-03-30 16:43:51 +0545

    feat: folder structure added..

---
+++ src/data/data.js
@@ -0,0 +1,75 @@
+export const productList = [
+    {
+      id: 1,
+      name: "cloth",
+      price: 1234,
+      img: "https://img.freepik.com/free-photo/blue-t-shirt_125540-727.jpg",
+    },
+    {
+      id: 2,
+      name: "pant",
+      price: 1234,
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-dressclothingwomen-dressfashion-women-dress-cloth-apparel-631522326975ia8xr.png",
+    },
+    {
+      id: 3,
+      name: "t-shirt",
+      price: 1234,
+      img: "https://image.pngaaa.com/83/763083-middle.png",
+    },
+    {
+      id: 4,
+      name: "product",
+      price: 1234,
+      img: "https://freepngimg.com/thumb/dress_shirt/4-black-dress-shirt-png-image-thumb.png",
+    },
+    {
+      id: 5,
+      name: "product",
+      price: 1234,
+      img: "https://img.freepik.com/free-photo/blue-t-shirt_125540-727.jpg",
+    },
+    {
+      id: 6,
+      name: "product",
+      price: 1234,
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-dressclothingwomen-dressfashion-women-dress-cloth-apparel-631522326975ia8xr.png",
+    },
+    {
+      id: 7,
+      name: "product",
+      price: 1234,
+      img: "https://img.freepik.com/free-photo/blue-t-shirt_125540-727.jpg",
+    },
+    {
+      id: 8,
+      name: "product",
+      price: 1234,
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-dressclothingwomen-dressfashion-women-dress-cloth-apparel-631522326975ia8xr.png",
+    },
+    {
+      id: 9,
+      name: "product",
+      price: 1234,
+      img: "https://image.pngaaa.com/83/763083-middle.png",
+    },
+    {
+      id: 10,
+      name: "product",
+      price: 1234,
+      img: "https://freepngimg.com/thumb/dress_shirt/4-black-dress-shirt-png-image-thumb.png",
+    },
+    {
+      id: 11,
+      name: "product",
+      price: 1234,
+      img: "https://img.freepik.com/free-photo/blue-t-shirt_125540-727.jpg",
+    },
+    {
+      id: 12,
+      name: "product",
+      price: 1234,
+      img: "https://purepng.com/public/uploads/large/purepng.com-women-dressclothingwomen-dressfashion-women-dress-cloth-apparel-631522326975ia8xr.png",
+    },
+  ];
+  
